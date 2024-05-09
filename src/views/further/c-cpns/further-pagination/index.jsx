6f5b43dc0251e8e3e1a45a3967e4b93194d9136e
Linxi <1595818766@qq.com>
2024-05-09 20:52:17 +0800

    底部分页数据处理

--- src/views/further/c-cpns/further-pagination/index.jsx
+++ src/views/further/c-cpns/further-pagination/index.jsx
@@ -1,9 +1,48 @@
 import React, { memo } from 'react'
 import { PaginationWrapper } from './style'
+import Pagination from '@mui/material/Pagination'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchRoomListAction } from '@/store/modules/further/actionCreators'
 
 const FurtherPagination = memo(() => {
+
+  const { totalCount, currentPage, roomList } = useSelector((state) => ({
+    totalCount: state.further.totalCount,
+    currentPage: state.further.currentPage,
+    roomList: state.further.roomList
+  }))
+
+  const totalPage = Math.ceil(totalCount / 20)
+  const startCount = currentPage * 20 + 1
+  const endCount = (currentPage + 1) * 20
+
+
+  const dispatch=useDispatch()
+  function pageChangeHandle(event, newPage){
+    window.scrollTo(0, 0)
+    dispatch(fetchRoomListAction(newPage-1))
+  }
+
   return (
-    <PaginationWrapper>FurtherPagination</PaginationWrapper>
+    <PaginationWrapper>
+      {
+        // roomList.length初始化未0会显示出来因此!!转化为布尔值进行判断显示
+        !!roomList.length && (
+          <div className='info'>
+            {/* 修改样式也可以用类的方式去覆盖内容 */}
+            <Pagination  onChange={pageChangeHandle} count={totalPage} sx={{
+              '& .Mui-selected': {
+                backgroundColor: '#222 !important', // 设置选中页码的背景色
+                color: 'white !important', // 设置选中页码的文本颜色
+              },
+            }} />
+            <div className='desc'>
+              第 {startCount} - {endCount}个房源, 共超过{totalCount}个
+            </div>
+          </div>
+        )
+      }
+    </PaginationWrapper>
   )
 })
 
--- src/views/further/c-cpns/further-pagination/style.js
+++ src/views/further/c-cpns/further-pagination/style.js
@@ -2,6 +2,39 @@ import styled from "styled-components";
 
 
 export const PaginationWrapper = styled.div`
- 
- 
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+
+  .info{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .desc{
+      display: flex;
+      align-items: center;
+      margin-top: 16px;
+    }
+  }
+
+  .MuiPaginationItem-icon {
+    font-size: 20px;
+  }
+
+  .MuiPaginationItem-page{
+    margin: 0 9px;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  .MuiPaginationItem-page.Mui-selected {
+    background-color: #222;
+    color: #fff;
+
+    &:hover {
+      background-color: #222;
+    }
+  }
  `
