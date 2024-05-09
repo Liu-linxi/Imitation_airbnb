1bcee25bc0135ae5461c44032372ab5f52dfb7f5
linxi <1595818766@qq.com>
2024-05-09 16:47:47 +0800

    房间列表数据获取

--- src/store/modules/further/actionCreators.js
+++ src/store/modules/further/actionCreators.js
@@ -26,14 +26,18 @@ export const changeIsLoadingAction = (isLoading) => ({
 export const fetchRoomListAction = (page = 0) => {
   // 新的函数
   return async (dispatch, getState) => {
+
     // 0.修改currentPage
-    dispatch(changeCurrentPageAction(page))
+    // dispatch(changeCurrentPageAction(page))
 
     // 1.根据页码获取最新的数据
-    // const currentPage = getState().entire.currentPage
-    dispatch(changeIsLoadingAction(true))
-    const res = await getFurtherRoomList(page * 20)
-    dispatch(changeIsLoadingAction(false))
+    const currentPage = getState().further.currentPage
+    console.log(currentPage)
+    const res = await getFurtherRoomList(currentPage * 20)
+
+    // dispatch(changeIsLoadingAction(true))
+    // const res = await getFurtherRoomList(page * 20)
+    // dispatch(changeIsLoadingAction(false))
 
     // 2.获取到最新的数据, 保存redux的store中
     const roomList = res.list
--- src/views/further/index.jsx
+++ src/views/further/index.jsx
@@ -1,15 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 import { FutherWrapper } from './style'
 import FurtherFilter from './c-cpns/further-filter'
 import FurtherRooms from './c-cpns/further-rooms'
 import FurtherPagination from './c-cpns/further-pagination'
 
+import { getFurtherRoomList } from '@/services/modules/further'
+import { useDispatch } from 'react-redux'
+import { fetchRoomListAction } from '@/store/modules/further/actionCreators'
+
 const further = memo(() => {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchRoomListAction(2))
+  })
+
   return (
     <FutherWrapper>
       <FurtherFilter></FurtherFilter>
       <FurtherRooms></FurtherRooms>
-      <FurtherPagination></FurtherPagination> 
+      <FurtherPagination></FurtherPagination>
     </FutherWrapper>
   )
 })
