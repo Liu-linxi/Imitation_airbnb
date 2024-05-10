e55a6098c7d5aaef22025a95bb7824a24bb20dba
linxi <1595818766@qq.com>
2024-05-10 15:34:32 +0800

    房间内容指示器

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,20 +1,31 @@
 import PropTypes from 'prop-types'
-import React, { memo, useRef } from 'react'
+import React, { memo, useRef, useState } from 'react'
 import { Rating } from '@mui/material'
 import { ItemWrapper } from './style'
 import { Carousel } from 'antd'
 import IconArrowLeft from '@/assets/svg/icon-arrow-left'
 import IconArrowRight from '@/assets/svg/icon-arrow-right'
+import Indicator from '@/base-ui/indicator'
+import classNames from 'classnames'
 
 const RoomItem = memo((props) => {
 
   const { itemData, itemWidth, itemPadding } = props
+  const [selectIndex, setSelectIndex] = useState(0)
+
   const swiperRef = useRef()
 
 
   /**左右控制器显示处理 */
   function controlClick(isLeft = true) {
     isLeft ? swiperRef.current.prev() : swiperRef.current.next()
+
+    // 获取最新的索引
+    let newIndex = isLeft ? selectIndex - 1 : selectIndex + 1;
+    const pictureUrlsLenght = itemData.picture_urls.length
+    if (newIndex < 0) newIndex = pictureUrlsLenght - 1;
+    if (newIndex > pictureUrlsLenght - 1) newIndex = 0;
+    setSelectIndex(newIndex)
   }
 
 
@@ -32,6 +43,17 @@ const RoomItem = memo((props) => {
             <div className='btn right' onClick={e => controlClick(false, e)}>
               <IconArrowRight width={22} height={22} />
             </div>
+          </div>
+          <div className='indicator'>
+            <Indicator selectIndex={selectIndex}>
+              {
+                itemData?.picture_urls?.map((item, index) => (
+                  <div className="item" key={index}>
+                    <span className={classNames("dot", { active: selectIndex === index })}></span>
+                  </div>
+                ))
+              }
+            </Indicator>
           </div>
           <Carousel dots={false} ref={swiperRef}>
             {
