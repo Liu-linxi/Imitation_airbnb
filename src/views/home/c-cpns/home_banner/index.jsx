92db30fb1f4c35bd30aee1dd2eb3e6edf1ee7c70
linxi <1595818766@qq.com>
2024-05-13 17:50:08 +0800

    节流推送

--- src/views/home/c-cpns/home_banner/index.jsx
+++ src/views/home/c-cpns/home_banner/index.jsx
@@ -1,6 +1,6 @@
 import React, { memo } from 'react'
 import { BannerWrapper } from './style'
-import coverImg from '@/assets/img/cover_01.jpeg'
+// import coverImg from '@/assets/img/cover_01.jpeg'
 
 
 const HomeBanner = memo(() => {
